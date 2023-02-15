61d3a8dac86d21145d9b7d524b2cc1b4e4686d0e
BernardAli <alibernard.coding@gmail.com>
2023-02-15 10:09:58 +0000

    Initial Commit

--- pdf.py
+++ pdf.py
@@ -1,7 +1,7 @@
 import camelot
 
-tables = camelot.read_pdf("Test.pdf", pages='59')
+tables = camelot.read_pdf("Test.pdf", pages='68')
 print(tables)
 
 tables.export('foot.csv', f='csv', compress=True)
-tables[0].to_csv('foo.csv')
+tables[1].to_csv('foo.csv')
